3d07bb18d6d93fb15beb94c5a215a30b401cdfd3
Charles Levine <charleslevine@NetSci-6.local>
2026-04-14 13:48:11 -0400

    Ignore 1-Vaious_PDE_and_Chat_stuff folder

--- 1-Various_PDE_and_Chat_stuff/Helpful_code.py
+++
@@ -1,3 +0,0 @@
-df.replace(to_replace=None, value=None, inplace=False, regex=False) ## Replace every instance iof an object in a dataframe
-string_exists = df.astype(str).applymap(lambda x: string_to_find in x).any().any() ## Any match
-df.isin([string_to_find]).any().any() ## Exact Match
